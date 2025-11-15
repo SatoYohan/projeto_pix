708cb05ebf95ac551ddd87dace4704d2e10e55ad
Abner Yohan Sato <102881830+SatoYohan@users.noreply.github.com>
2025-11-14 23:12:19 -0300

    feat(server): novos itens para entrega 2

--- src/main/java/com/projeto_pix/server/Server.java
+++ src/main/java/com/projeto_pix/server/Server.java
@@ -1,26 +1,36 @@
 // Em: src/main/java/com/projeto_pix/server/Server.java
 package com.projeto_pix.server;
 
-import java.net.InetAddress; // 1. Importe a classe InetAddress
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger; // Importar para o ID da transação
 import com.projeto_pix.common.model.Usuario;
+import com.projeto_pix.common.model.Transacao; // Importar o modelo de Transacao
 
 public class Server {
     // Usamos ConcurrentHashMap para segurança em ambiente com múltiplas threads
     // static para que sejam compartilhados entre todas as instâncias de ClientHandler
     public static final Map<String, Usuario> usuarios = new ConcurrentHashMap<>();
-    public static final Map<String, String> sessoesAtivas = new ConcurrentHashMap<>(); // Mapeia token -> cpf
+    public static final Map<String, String> sessoesAtivas = new ConcurrentHashMap<>();
+
+    // Lista segura para threads para armazenar todas as transações (Extrato)
+    public static final List<Transacao> transacoes = Collections.synchronizedList(new ArrayList<>());
+    // Contador atômico para garantir IDs de transação únicos
+    public static final AtomicInteger transacaoIdCounter = new AtomicInteger(1);
 
     public static void main(String[] args) {
-        final int PORT = 12345;
+        final int PORT = 24000;
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
 
             try {
-                // 2. Obtém o endereço de IP da máquina local
+                // Obtém o endereço de IP da máquina local
                 InetAddress ip = InetAddress.getLocalHost();
                 System.out.println("=================================================");
                 System.out.println("Servidor iniciado!");
